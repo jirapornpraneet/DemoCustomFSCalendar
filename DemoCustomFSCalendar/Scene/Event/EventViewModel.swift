581bba05261e493000eb8da72955bae870e9533f
iOS Dev <jiraporn.p@piesoft.net>
2021-10-08 16:24:37 +0700

    edit data event

--- DemoCustomFSCalendar/Scene/Event/EventViewModel.swift
+++ DemoCustomFSCalendar/Scene/Event/EventViewModel.swift
@@ -27,11 +27,11 @@ extension EventViewModel {
 
     func fetchMenu(_ completion: (() -> ())? = nil) {
         
-        eventsInfo.append(EventInfo(id:1,dateStart: "2021-10-04T00:00:00+07:00", dateEnd: "2021-10-04T23:59:59+07:00", typeId: 1, title: ""))
+        eventsInfo.append(EventInfo(id:1,dateStart: "2021-10-04T00:00:00+07:00", dateEnd: "2021-10-04T23:59:59+07:00", typeId: 1, title: "ลาป่วย"))
         
-        eventsInfo.append(EventInfo(id:2,dateStart: "2021-10-12T00:00:00+07:00", dateEnd: "2021-05-14T23:59:59+07:00", typeId: 2, title: ""))
+        eventsInfo.append(EventInfo(id:2,dateStart: "2021-10-12T00:00:00+07:00", dateEnd: "2021-10-15T00:00:00+07:00", typeId: 2, title: "ลากิจ"))
         
-        eventsInfo.append(EventInfo(id:3,dateStart: "2021-10-30T00:00:00+07:00", dateEnd: "2021-05-31T23:59:59+07:00", typeId: 3, title: ""))
+        eventsInfo.append(EventInfo(id:3,dateStart: "2021-10-28T00:00:00+07:00", dateEnd: "2021-10-31T00:00:00+07:00", typeId: 3, title: "ลาพักผ่อน"))
         
         
         completion?()
